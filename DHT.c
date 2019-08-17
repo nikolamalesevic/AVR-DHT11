06bceedfc0f6eee5cfd5d6240a41d635388e06c8
Nikola Malesevic <nikola.malesevic@gmail.com>
2019-08-17 18:14:07 +0200

    Use a timer instead of delay (WIP)

--- DHT.c
+++ DHT.c
@@ -1,138 +1,217 @@
-#include "DHT.h"
+ #include "DHT.h"
 
+#include <avr/interrupt.h>
 #include <avr/io.h>
-#include <util/delay.h>
+#include <math.h>
+#include <stdbool.h>
+#include "io_helper.h"
+#include "time.h"
 
-int main(void)
+#define PORT  D
+#define PIN   2
+
+typedef enum
 {
-    uint8_t data [4];
-    
-    int8_t i;
-    
-    initDHT();
-    
-    if(fetchData(data))
-    {
-        for(i = data[2]; i >= 0; --i)
-        {
-            SET_BIT(PORTB,LED);
-            _delay_ms(100);
-            
-            CLEAR_BIT(PORTB,LED);
-            _delay_ms(500);
-        }
-    }
+  ST_IDLE,
+  ST_ERROR,
+  ST_ERROR_CHECKSUM,
+  ST_BIT_CLEARED,
+  ST_WAIT_FOR_RESPONSE,
+  ST_RESPONSE_STARTED,
+  ST_RESPONSE_COMPLETED,
+  ST_BYTE_TRANSFER_STARTING,
+  ST_BIT_TRANSFER_STARTING,
+  ST_BIT_INCOMING,
+  ST_COMPLETED
+} State;
 
-    return 0;
+volatile float dht_humidity = 25.6;
+volatile float dht_temperature = 36.7;
+
+static volatile State state_;
+static volatile uint8_t timer_counter_;
+static volatile uint8_t byte_index_;
+static volatile uint8_t bit_index_;
+
+static volatile uint8_t data_[5];
+
+bool process_data();
+void read_data();
+
+void on_second();
+
+void dht_init()
+{
+  state_ = ST_IDLE;
+  TCCR0 = (1 << WGM01); // Timer CTC mode.
+  time_subscribe_s(&on_second);
 }
 
-void initDHT(void)
+bool process_data()
 {
-    /* Set LED as output */
-    SET_BIT(DDRB,LED);
-    
-    /* According to the DHT11's datasheet, the sensor needs about
-       1-2 seconds to get ready when first getting power, so we
-       wait
-     */
-    _delay_ms(2000);
+  uint8_t checksum = 0;
+
+  for (uint8_t i = 0; i < 4; ++i)
+    checksum += data_[i];
+
+  if (data_[4] != checksum)
+  {
+    state_ = ST_ERROR_CHECKSUM;
+    return false;
+  }
+
+  dht_humidity = data_[0] + data_[1] * 0.1;
+  dht_temperature = data_[2] + data_[3] * 0.1;
+  return true;
 }
 
-
-uint8_t fetchData(uint8_t* arr)
+void read_data()
 {
-    uint8_t data [5];
-    uint8_t cnt, check;
-    int8_t i,j;
+  // Set pin as output and low.
+  IO_DIRECTION_OUT(PORT, PIN);
+  PORTx_OFF(PORT, PIN);
+
+  // Set up timer.
+  state_ = ST_BIT_CLEARED;
+  TCCR0 = (1 << WGM01) | (1 << CS02) | (1 << CS00); // Set F_CPU/1024 prescaler.
+  OCR0 = 144; // Wait for 20 ms.
+  TCNT0 = 0;
+  TIFR |= (1 << OCF0);
+  TIMSK |= (1 << OCIE0); // Enable CTC interrupt for the timer.
+  TIFR |= (1 << OCF0);
+  sei(); // Enable global interrupts.
+}
+
+void on_second()
+{
+  //if (time_s % 5 == 0)
+    read_data();
+}
+
+ISR(TIMER0_COMP_vect)
+{
+  timer_counter_++;
+
+  switch (state_)
+  {
+    case ST_BIT_CLEARED:
+      // Set pin as input.
+      PORTx_ON(PORT, PIN);
+      IO_DIRECTION_IN(PORT, PIN);
+
+      // Wait for response to start.
+      state_ = ST_WAIT_FOR_RESPONSE;
+      timer_counter_ = 0;
+      TCCR0 = (1 << WGM01) | (1 << CS01); // Set F_CPU/8 prescaler.
+      //TCNT0 = 0;
+      OCR0 = 2;
+      //TIMSK &= ~(1 << OCIE0);
+      break;
+
+    case ST_WAIT_FOR_RESPONSE:
+      // Wait for pin to become unset (40 µs max).
+      if (timer_counter_ >= 37) { /*dht_humidity++; */state_ = ST_ERROR; break; } // Timeout at 40 µs.
+
+      if (!PINx(PORT, PIN))
+      {
+        dht_humidity = timer_counter_;
+        // Response started. Wait for response to finish.
+        state_ = ST_RESPONSE_STARTED;
+        timer_counter_ = 0;
+        TCNT0 = 0;
+      }
+
+      break;
     
-    /******************* Sensor communication start *******************/
-    
-    /* Set data pin as output first */
-    SET_BIT(DDRB,DHT_PIN);
-    
-    /* First we need milliseconds delay, so set clk/1024 prescaler */
-    TCCR0B = 0x05;
-    
-    TCNT0 = 0;
-    
-    /* Clear bit for 20 ms */
-    CLEAR_BIT(DHT_PORT_OUT,DHT_PIN);
-    
-    /* Wait about 20 ms */
-    while(TCNT0 < 160);
-    
-    /* Now set Timer0 with clk/8 prescaling.
-     Gives 1µs per cycle @8Mhz */
-    TCCR0B = 0x02;
-    
-    TCNT0 = 0;
-    
-    /* Pull high again */
-    SET_BIT(DHT_PORT_OUT,DHT_PIN);
-    
-    /* And set data pin as input */
-    CLEAR_BIT(DDRB,DHT_PIN);
-    
-    /* Wait for response from sensor, 20-40µs according to datasheet */
-    while(IS_SET(DHT_PORT_IN,DHT_PIN))
-    { if (TCNT0 >= 60) return 0; }
-    
-    /************************* Sensor preamble *************************/
-    
-    TCNT0 = 0;
-    
-    /* Now wait for the first response to finish, low ~80µs */
-    while(!IS_SET(DHT_PORT_IN,DHT_PIN))
-    { if (TCNT0 >= 100) return 0; }
-    
-    TCNT0 = 0;
-    
-    /* Then wait for the second response to finish, high ~80µs */
-    while(IS_SET(DHT_PORT_IN,DHT_PIN))
-    { if (TCNT0 >= 100) return 0; }
-    
-    /********************* Data transmission start **********************/
-    
-    for (i = 0; i < 5; ++i)
-    {
-        for(j = 7; j >= 0; --j)
-        {
-            TCNT0 = 0;
-            
-            /* First there is always a 50µs low period */
-            while(!IS_SET(DHT_PORT_IN,DHT_PIN))
-            { if (TCNT0 >= 70) return 0; }
-            
-            TCNT0 = 0;
-            
-            /* Then the data signal is sent. 26 to 28µs (ideally)
-             indicate a low bit, and around 70µs a high bit */
-            while(IS_SET(DHT_PORT_IN,DHT_PIN))
-            { if (TCNT0 >= 90) return 0; }
-            
-            /* Store the value now so that the whole checking doesn't
-             move the TCNT0 forward by too much to make the data look
-             bad */
-            cnt = TCNT0;
-            
-            if (cnt >= 20 && cnt <= 35)
-            { CLEAR_BIT(data[i],j); }
-            
-            else if (cnt >= 60 && cnt <= 80)
-            { SET_BIT(data[i],j); }
-            
-            else return 0;
-        }
-    }
-    
-    /********************* Sensor communication end *********************/
-    
-    check = (data[0] + data[1] + data[2] + data[3]) & 0xFF;
-    
-    if (check != data[4]) return 0;
-    
-    for(i = 0; i < 4; ++i)
-    { arr[i] = data[i]; }
-    
-    return 1;
+    case ST_RESPONSE_STARTED:
+      // Wait for pin to become set (80 µs ideally).
+      if (timer_counter_ >= 93) { dht_humidity = 102; state_ = ST_ERROR; break; } // Timeout at 100 µs.
+
+      if (PINx(PORT, PIN))
+      {
+        dht_temperature = timer_counter_;
+        // Response completed. Wait for data transfer start.
+        state_ = ST_RESPONSE_COMPLETED;
+        timer_counter_ = 0;
+        TCNT0 = 0;
+        TIMSK &= ~(1 << OCIE0);
+      }
+
+      break;
+
+    case ST_RESPONSE_COMPLETED:
+      // Wait for pin to become unset again (80 µs ideally).
+      if (timer_counter_ >= 93) { dht_humidity = 103; state_ = ST_ERROR; break; } // Timeout at 100 µs.
+
+      if (!PINx(PORT, PIN))
+      {
+        // Data transfer is about to start.
+        state_ = ST_BYTE_TRANSFER_STARTING;
+        timer_counter_ = 0;
+        byte_index_ = 0;
+        TCNT0 = 0;
+      }
+
+      break;
+
+    /** Single byte processing. */
+
+    case ST_BYTE_TRANSFER_STARTING:
+      if (byte_index_ > 5)
+      {
+        if (process_data())
+          state_ = ST_COMPLETED;
+
+        break;
+      }
+
+      bit_index_ = 0;
+      state_ = ST_BIT_TRANSFER_STARTING;
+
+    case ST_BIT_TRANSFER_STARTING:
+      // Wait during low period for bit start (50 µs ideally).
+      if (timer_counter_ >= 56) { dht_humidity = 104; state_ = ST_ERROR; break; } // Timeout at 60 µs.
+
+      if (!PINx(PORT, PIN))
+        break;
+      
+      // Data incoming.
+      state_ = ST_BIT_INCOMING;
+      timer_counter_ = 0;
+      TCNT0 = 0;
+      break;
+
+    case ST_BIT_INCOMING:
+      // Wait for bit end. 26 to 28 µs indicate low bit, 70 µs indicate high bit.
+      if (timer_counter_ >= 78) { dht_humidity = 105; state_ = ST_ERROR; break; } // Timeout at 84 µs.
+
+      if (PINx(PORT, PIN))
+        break;
+
+      // Bit ended.
+      if (timer_counter_ >= 19 && timer_counter_ <= 31) // Between 20.80 and 33.60 µs.
+        data_[byte_index_] &= ~(1 << bit_index_);
+      else if (timer_counter_ >= 51 && timer_counter_ <= 78) // Between 56 and 84 µs.
+        data_[byte_index_] |= (1 << bit_index_);
+      else
+      {
+        //dht_humidity = 106;
+        //dht_temperature = timer_counter_;
+        state_ = ST_ERROR; // Erroneous bit.
+        break;
+      }
+
+      if (++bit_index_ >= 8)
+      {
+        byte_index_++;
+        state_ = ST_BYTE_TRANSFER_STARTING;
+      }
+
+      timer_counter_ = 0;
+      TCNT0 = 0;
+      break;
+
+    default:
+      break;
+  }
 }
--- DHT.h
+++ DHT.h
@@ -1,24 +1,15 @@
 #ifndef _DHT_h
 #define _DHT_h
 
-#define F_CPU 8000000UL
-
-#define DHT_PORT_OUT PORTB
-#define DHT_PORT_IN PINB
-#define DHT_PIN PB3
-
-#define LED PB4
-
-#define SET_BIT(byte, bit) ((byte) |= (1UL << (bit)))
-
-#define CLEAR_BIT(byte,bit) ((byte) &= ~(1UL << (bit)))
-
-#define IS_SET(byte,bit) (((byte) & (1UL << (bit))) >> (bit))
-
 #include <stdint.h>
 
-void initDHT(void);
+extern volatile float dht_humidity;
+extern volatile float dht_temperature;
 
-uint8_t fetchData(uint8_t* arr);
+/**
+ * Initializes DHT sensor.
+ * The sensor needs at least 2000 ms to start up after initial power-up.
+ */
+void dht_init();
 
 #endif
